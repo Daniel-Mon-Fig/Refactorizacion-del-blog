1375cd650203322ab704caace838d163a1a25fe7
Daniel-Mon-Fig <danielmonfig94@gmail.com>
2026-07-08 13:07:18 -0600

    Subo actualización

--- script.js
+++ script.js
@@ -2,3 +2,23 @@
   AD-14 - JavaScript para la página de contacto.
   Objetivo: mostrar una alerta Bootstrap cuando el usuario presione el botón Enviar.
 */
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-light alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Mensaje enviado', 'success')
+  })
+}
